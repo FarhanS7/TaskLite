69f254e0a9d020d1fcbe34a75aa91ca42b0b1da2
Farhan <ullah8113@gmail.com>
2025-08-07 19:50:02 +0600

    Task Board Ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <TaskBoard />
       <Footer />
     </>
   );
