ef59e8ec781ab22d50531630227891bcbb6c3c53
Farhan <ullah8113@gmail.com>
2025-08-08 11:35:46 +0600

    AddTask Modal added

--- src/task/TaskActions.jsx
+++ src/task/TaskActions.jsx
@@ -1,10 +1,13 @@
-export const TaskActions = () => {
+export const TaskActions = ({ onAddClick }) => {
   return (
     <>
       <div className="mb-14 items-center justify-between sm:flex">
         <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
         <div className="flex items-center space-x-5">
-          <button className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold">
+          <button
+            onClick={onAddClick}
+            className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold"
+          >
             Add Task
           </button>
           <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
@@ -15,3 +18,9 @@ export const TaskActions = () => {
     </>
   );
 };
+
+// If we need to use the state from the parent component in the child component,
+//  we can pass the state as a prop to the child component.
+//  But if we need to use the state from the child component in the parent component,
+//  we can pass a function as a prop to the child component that allows it to
+//  update the state in the parent component.
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,19 +1,36 @@
+import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import { SearchTask } from "./SearchTask";
 import { TaskActions } from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  const defaultTasks = {
+    id: 1,
+    title: "Task asdfas",
+    description: "Description for Task 1",
+    tags: ["tag1", "tag2"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTasks]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask() {
+    setShowAddModal(true);
+  }
+
   return (
     <>
       <section className="mb-20" id="tasks">
+        {showAddModal && <AddTaskModal />}
         <div className="container">
           <div className="p-2 flex justify-end">
             <SearchTask />
           </div>
-
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskActions />
-            <TaskList />
+            <TaskActions onAddClick={handleAddTask} />
+            <TaskList tasks={tasks} />
           </div>
         </div>
       </section>
